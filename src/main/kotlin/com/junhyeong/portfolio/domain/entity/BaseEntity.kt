09319f761f247d603d956ff18adb97d5ab2d213c
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2024-02-19 03:03:13 +0900

    Entity - no relation

--- src/main/kotlin/com/junhyeong/portfolio/domain/entity/BaseEntity.kt
+++ src/main/kotlin/com/junhyeong/portfolio/domain/entity/BaseEntity.kt
@@ -1,8 +1,18 @@
 package com.junhyeong.portfolio.domain.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
 @MappedSuperclass // 이 어노테이션을 사용해야 상속 받을 수 있음.
 abstract class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false) // null이 있을 수 없고, update 할 수 없음
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
 
+    @LastModifiedDate
+    @Column(nullable = false, updatable = true) // data 수정이 있을 때마다 update time이 바껴야하므로 true
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
 }
--- src/main/kotlin/com/junhyeong/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/junhyeong/portfolio/domain/entity/Skill.kt
@@ -1,16 +1,19 @@
 package com.junhyeong.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.junhyeong.portfolio.domain.constant.SkillType
+import jakarta.persistence.*
 
 @Entity
-class Skill: BaseEntity() {
+class Skill(name: String, type: String, isActive: Boolean): BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
+    val name: String = name
+
+    @Column(name="skill_type") // oracle에서는 type이라는 예약어와 겹칠 수 있음
+    @Enumerated(value = EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type) // constant에 만들어 놓은 enum을 찾아서 사용하는 것임
+    var isActive: Boolean = isActive
 
 }
