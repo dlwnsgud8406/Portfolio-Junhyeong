78788ba33c143252cc1ec4eda7c5eb4d74df6ddf
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2024-02-20 05:44:19 +0900

    Thymeleaf - projects template

--- src/main/kotlin/com/junhyeong/portfolio/domain/DataInitializer.kt
+++ src/main/kotlin/com/junhyeong/portfolio/domain/DataInitializer.kt
@@ -18,22 +18,24 @@ class DataInitializer(private val achievementRepository: AchievementRepository,
     private val experienceRepository: ExperienceRepository) {
 
     @PostConstruct
-    fun initializeData(){
-        println("스프링이 실행되었습니다. 테스트 데이터를 초기화합니다.")
+    fun initializeData() {
 
+        // achievement 초기화
         val achievements = mutableListOf<Achievement>(
             Achievement(
                 title = "2022 Catkao 해커톤 최우수상",
-                description = "고양이 쇼핑몰 검색 서비스의 아키텍처, 데이터 모델링, API 개발 역할 수행", host = "캣카오",
+                description = "고양이 쇼핑몰 검색 서비스의 아키텍처, 데이터 모델링, API 개발 역할 수행",
+                host = "캣카오",
                 achievedDate = LocalDate.of(2022, 8, 1),
                 isActive = true
             ),
             Achievement(
                 title = "정보처리기사",
-                description = "자료구조, 운영체제, 알고리즘, 데이터베이스 등", host = "한국산업인력공단",
+                description = "자료구조, 운영체제, 알고리즘, 데이터베이스 등",
+                host = "한국산업인력공단",
                 achievedDate = LocalDate.of(2020, 2, 2),
                 isActive = true
-            ),
+            )
         )
         achievementRepository.saveAll(achievements)
 
@@ -45,45 +47,47 @@ class DataInitializer(private val achievementRepository: AchievementRepository,
         )
         introductionRepository.saveAll(introductions)
 
+        // link 초기화
         val links = mutableListOf<Link>(
-            Link(name = "Github", content = "https://github.com/dlwnsgud8406", isActive = true),
-            Link(name = "Linkedin", content = "https://www.linkedin.com/in/bokeunjeong",
-                isActive = true),
+            Link(name = "Github", content = "https://github.com/infomuscle", isActive = true),
+            Link(name = "Linkedin", content = "https://www.linkedin.com/in/bokeunjeong", isActive = true),
         )
         linkRepository.saveAll(links)
 
         // experience / experience_detail 초기화
         val experience1 = Experience(
-        title = "캣홀릭대학교(CatHolic Univ.)", description = "컴퓨터공학 전공", startYear = 2018,
-        startMonth = 9,
-        endYear = 2022,
-        endMonth = 8,
-
-        isActive = true,
+            title = "캣홀릭대학교(CatHolic Univ.)",
+            description = "컴퓨터공학 전공",
+            startYear = 2018,
+            startMonth = 9,
+            endYear = 2022,
+            endMonth = 8,
+            isActive = true
         )
         experience1.addDetails(
             mutableListOf(
                 ExperienceDetail(content = "GPA 4.3/4.5", isActive = true),
                 ExperienceDetail(content = "소프트웨어 연구 학회 활동", isActive = true)
             )
-            )
-
+        )
         val experience2 = Experience(
-        title = "주식회사 캣카오(Catkao Corp.)", description = "소셜서비스팀 백엔드 개발자", startYear = 2022,
-        startMonth = 9,
-        endYear = null,
-        endMonth = null,
-        isActive = true,
+            title = "주식회사 캣카오(Catkao Corp.)",
+            description = "소셜서비스팀 백엔드 개발자",
+            startYear = 2022,
+            startMonth = 9,
+            endYear = null,
+            endMonth = null,
+            isActive = true
         )
         experience2.addDetails(
             mutableListOf(
-                ExperienceDetail(content = "유기묘 위치 공유 서비스 개발", isActive = true), ExperienceDetail(content = "신입 교육 프로그램 우수상 수상", isActive = true)
+                ExperienceDetail(content = "유기묘 위치 공유 서비스 개발", isActive = true),
+                ExperienceDetail(content = "신입 교육 프로그램 우수상 수상", isActive = true)
             )
         )
         experienceRepository.saveAll(mutableListOf(experience1, experience2))
 
-
-// skill 초기화
+        // skill 초기화
         val java = Skill(name = "Java", type = SkillType.LANGUAGE.name, isActive = true)
         val kotlin = Skill(name = "Kotlin", type = SkillType.LANGUAGE.name, isActive = true)
         val python = Skill(name = "Python", type = SkillType.LANGUAGE.name, isActive = true)
@@ -95,15 +99,15 @@ class DataInitializer(private val achievementRepository: AchievementRepository,
         skillRepository.saveAll(mutableListOf(java, kotlin, python, spring, django, mysql, redis, kafka))
 
         // project / project_detail / project_skill 초기화
-        val project1 = Project(
-        name = "유기묘 발견 정보 공유 서비스",
-        description = "유기묘 위치의 실시간 공유, 임시보호까지 연결해주는 서비스. 구글 맵스를 연동하여 유기묘 위치 정보를 직관적으로 파악할 수 있도록 하는 사용자 경험 개선 작업.",
-        startYear = 2022,
-        startMonth = 9,
-        endYear = 2022,
-        endMonth = 12,
 
-        isActive = true
+        val project1 = Project(
+            name = "유기묘 발견 정보 공유 서비스",
+            description = "유기묘 위치의 실시간 공유, 임시보호까지 연결해주는 서비스. 구글 맵스를 연동하여 유기묘 위치 정보를 직관적으로 파악할 수 있도록 하는 사용자 경험 개선 작업.",
+            startYear = 2022,
+            startMonth = 9,
+            endYear = 2022,
+            endMonth = 12,
+            isActive = true
         )
         project1.addDetails(
             mutableListOf(
@@ -130,10 +134,8 @@ class DataInitializer(private val achievementRepository: AchievementRepository,
         )
         project2.addDetails(
             mutableListOf(
-                ProjectDetail(content = "PIL(Pillow) 활용하여 이미지 분석 기능 개발", url = null,
-                    isActive = true),
-                ProjectDetail(content = "알림 발송을 비동기 처리하여 이미지 분석 - 알림 발송 기능간 의존도 감소", url = null,
-                    isActive = true),
+                ProjectDetail(content = "PIL(Pillow) 활용하여 이미지 분석 기능 개발", url = null, isActive = true),
+                ProjectDetail(content = "알림 발송을 비동기 처리하여 이미지 분석 - 알림 발송 기능간 의존도 감소", url = null, isActive = true),
                 ProjectDetail(content = "Github Repository", url = "https://github.com/infomuscle", isActive = true)
             )
         )
@@ -142,7 +144,9 @@ class DataInitializer(private val achievementRepository: AchievementRepository,
                 ProjectSkill(project = project2, skill = python),
                 ProjectSkill(project = project2, skill = django),
                 ProjectSkill(project = project2, skill = kafka)
-            ) )
+            )
+        )
         projectRepository.saveAll(mutableListOf(project1, project2))
+
     }
 }
--- src/main/kotlin/com/junhyeong/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/junhyeong/portfolio/domain/repository/ProjectRepository.kt
@@ -1,8 +1,5 @@
 package com.junhyeong.portfolio.domain.repository
 
-import com.junhyeong.portfolio.domain.entity.Achievement
-import com.junhyeong.portfolio.domain.entity.Experience
-import com.junhyeong.portfolio.domain.entity.Introduction
 import com.junhyeong.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
