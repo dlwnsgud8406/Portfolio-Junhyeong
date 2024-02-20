3c506604ca23c52b1496672e03a7a60ffb47d006
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2024-02-21 04:17:08 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 1:n

---
+++ src/main/kotlin/com/junhyeong/portfolio/admin/context/project/form/ProjectDetailForm.kt
@@ -0,0 +1,27 @@
+package com.junhyeong.portfolio.admin.context.project.form
+
+import com.junhyeong.portfolio.domain.entity.ExperienceDetail
+import com.junhyeong.portfolio.domain.entity.ProjectDetail
+import jakarta.validation.constraints.NotBlank
+
+data class ProjectDetailForm (
+    val id: Long,
+
+    @field:NotBlank(message = "필수값입니다.")
+    val content: String,
+
+    val url: String?,
+
+    val isActive: Boolean,
+
+
+) {
+    fun toEntity(): ProjectDetail {
+        return ProjectDetail(
+            content = this.content,
+            url = this.url,
+            isActive = this.isActive
+        )
+    }
+}
+
